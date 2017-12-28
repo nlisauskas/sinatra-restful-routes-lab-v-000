cbe82ed67f54f47a3a71257b94fea62500961954
Nick Lisauskas <nlisausk@alumni.nd.edu>
2017-12-28 16:47:16 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,6 +13,10 @@ class ApplicationController < Sinatra::Base
 
   post '/recipes' do
     @recipe = Recipe.create(:name => params[:name], :ingredients => params[:ingredients], :cook_time => params[:cook_time])
+    redirect '/recipes/:id'
+  end
+
+  get '/recipes/:id' do
     erb :show
   end
 end
