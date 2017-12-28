d61e32a02c35c8427d985fba5a44b77f93555bd6
Nick Lisauskas <nlisausk@alumni.nd.edu>
2017-12-28 17:32:27 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,12 +3,7 @@ class ApplicationController < Sinatra::Base
     set :public_folder, 'public'
     set :views, 'app/views'
   end
-
-  get '/' do
-    @recipes = Recipe.all
-    erb :index
-  end
-
+  
   get '/recipes' do
     @recipes = Recipe.all
     erb :index
